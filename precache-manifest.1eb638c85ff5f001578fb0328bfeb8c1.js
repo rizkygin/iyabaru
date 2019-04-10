2910aaf189fc7ca8f2e3251171209aec5d7324c9
rizkygin <rizkygin@gmail.com>
2019-04-10 23:26:49 +0700

    Updates

---
+++ precache-manifest.1eb638c85ff5f001578fb0328bfeb8c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/iyabaru/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "f71707586cd4ff558310",
+    "url": "/iyabaru/static/js/runtime~main.e75438eb.js"
+  },
+  {
+    "revision": "d3e226cd1f324430108f",
+    "url": "/iyabaru/static/js/main.634a621a.chunk.js"
+  },
+  {
+    "revision": "3c688a83c6a96b1bd515",
+    "url": "/iyabaru/static/js/2.f885ce20.chunk.js"
+  },
+  {
+    "revision": "d3e226cd1f324430108f",
+    "url": "/iyabaru/static/css/main.5f9effcb.chunk.css"
+  },
+  {
+    "revision": "b83609920a6e66222ce42c3a67f26718",
+    "url": "/iyabaru/index.html"
+  }
+];
